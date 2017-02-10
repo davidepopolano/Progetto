65f9f7d6856911c5892c83d57262b86a22df4e7a
Davide Popolano <davide@MacBook-Pro-di-Davide.local>
2017-02-10 16:30:09 +0100

    username&password

--- app/src/main/java/com/elis/ltm/progetto/LoginActivity.java
+++ app/src/main/java/com/elis/ltm/progetto/LoginActivity.java
@@ -15,6 +15,8 @@ public class LoginActivity extends Activity implements View.OnClickListener {
     EditText usernameET;
     EditText passwordET;
     Button loginBtn;
+    String usernameprova;
+    String passwordprova;
 
 
     @Override
@@ -27,29 +29,31 @@ public class LoginActivity extends Activity implements View.OnClickListener {
         passwordET = (EditText) findViewById(R.id.login_et_passw);
         loginBtn = (Button) findViewById(R.id.login_btn);
         loginBtn.setOnClickListener(this);
+        usernameprova ="davide@popolano.it";
+        passwordprova ="provapassword";
     }
     @Override
     public void onClick(View v) {
-        if(v.getId()== R.id.login_btn){
+        if(v.getId()== R.id.login_btn) {
             String name = usernameET.getText().toString();
             String password = passwordET.getText().toString();
-
             if (onLogin(name, password)) {
                 Toast.makeText(LoginActivity.this, getString(R.string.login_succ), Toast.LENGTH_LONG).show();
                 Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                 startActivity(intent);
                 finish();
-            } else {
-                Toast.makeText(LoginActivity.this, "Something went wrong", Toast.LENGTH_SHORT).show();
             }
+
         }
-
-
     }
 
     public boolean onLogin(String username, String password) {
-
-        return true;
+        if(!username.equals(usernameprova)||!password.equals(passwordprova)) {
+            Toast.makeText(LoginActivity.this, getString(R.string.storto), Toast.LENGTH_SHORT).show();
+            return false;
+        }else{
+            return true;
+        }
     }
 
 
