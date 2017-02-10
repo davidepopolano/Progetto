cac73e10ef897b15f4dc2f50390832ceea57b864
Davide Popolano <davide@MacBook-Pro-di-Davide.local>
2017-02-10 16:07:57 +0100

    go&call_Button

--- app/src/main/java/com/elis/ltm/progetto/LoginActivity.java
+++ app/src/main/java/com/elis/ltm/progetto/LoginActivity.java
@@ -2,6 +2,7 @@ package com.elis.ltm.progetto;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.nfc.Tag;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,12 +10,11 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 
-public class LoginActivity extends Activity implements View.OnClickListener{
+public class LoginActivity extends Activity implements View.OnClickListener {
 
     EditText usernameET;
     EditText passwordET;
     Button loginBtn;
-
 
 
     @Override
@@ -27,39 +27,30 @@ public class LoginActivity extends Activity implements View.OnClickListener{
         passwordET = (EditText) findViewById(R.id.login_et_passw);
         loginBtn = (Button) findViewById(R.id.login_btn);
         loginBtn.setOnClickListener(this);
-
     }
-
-
-
     @Override
     public void onClick(View v) {
-        String name = usernameET.getText().toString();
-        String password = passwordET.getText().toString();
+        if(v.getId()== R.id.login_btn){
+            String name = usernameET.getText().toString();
+            String password = passwordET.getText().toString();
 
-        if(onLogin(name, password)) {
-            Toast.makeText(LoginActivity.this,getString(R.string.login_succ), Toast.LENGTH_LONG).show();
-            Intent intent = new Intent(LoginActivity.this,MainActivity.class);
-            startActivity(intent);
-            finish();
-        }else{
-            Toast.makeText(LoginActivity.this,"Something went wrong", Toast.LENGTH_SHORT).show();
+            if (onLogin(name, password)) {
+                Toast.makeText(LoginActivity.this, getString(R.string.login_succ), Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            } else {
+                Toast.makeText(LoginActivity.this, "Something went wrong", Toast.LENGTH_SHORT).show();
+            }
         }
 
+
     }
-    public boolean onLogin(String username, String password){
+
+    public boolean onLogin(String username, String password) {
 
         return true;
     }
-
-
-
-
-
-
-
-
-
 
 
 }
--- app/src/main/java/com/elis/ltm/progetto/MainActivity.java
+++ app/src/main/java/com/elis/ltm/progetto/MainActivity.java
@@ -1,20 +1,57 @@
 package com.elis.ltm.progetto;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener{
+
+    Button call;
+    Button go;
+    TextView numero;
+    TextView via;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_layout);
+        //view activity_main
+        setContentView(R.layout.activity_main);
+
+        call = (Button) findViewById(R.id.call_btn);
+        go = (Button) findViewById(R.id.go_btn);
+        numero = (TextView) findViewById(R.id.numerotelefono);
+        via = (TextView) findViewById(R.id.address);
+        call.setOnClickListener(this);
+        go.setOnClickListener(this);
+
     }
 
+
     @Override
-    public void finish() {
-        super.finish();
+    public void onClick(View v) {
+        if(v.getId() == R.id.call_btn){
+            Intent intent = new Intent();
+            intent.setAction(Intent.ACTION_VIEW);
+            Uri uri = Uri.parse("tel:" + numero.getText().toString());
+            intent.setData(uri);
+            startActivity(intent);
+        }else if(v.getId() == R.id.go_btn){
+
+            Intent intent = new Intent();
+            intent.setAction(Intent.ACTION_VIEW);
+            Uri uri = Uri.parse("geo:0,0?q=" + via.getText().toString());
+            intent.setData(uri);
+            startActivity(intent);
+        }
+
+
+
+
     }
 }
