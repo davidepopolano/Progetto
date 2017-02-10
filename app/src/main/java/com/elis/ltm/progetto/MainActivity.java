9da6246aeac55731109526167e3838593f71c276
Davide Popolano <davide@MacBook-Pro-di-Davide.local>
2017-02-10 10:38:16 +0100

    Progetto

---
+++ app/src/main/java/com/elis/ltm/progetto/MainActivity.java
@@ -0,0 +1,15 @@
+package com.elis.ltm.progetto;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.util.Log;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
